9d742e3b4ba97c9897336ddcaa738dcfa4140a70
Austin Roberts <d.austin.roberts@gmail.com>
2018-04-22 20:31:19 -0700

    Adding testing method but returns an error

--- LeapYear.Tests/ModelTests/LeapYearTest.cs
+++ LeapYear.Tests/ModelTests/LeapYearTest.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LeapYear.Models;
-
 namespace LeapYear.Tests
 {
   [TestClass]
@@ -11,7 +10,8 @@ namespace LeapYear.Tests
     [TestMethod]
     public void IsLeapYear_NumberDivisibleByFour_True()
     {
-      // eventually your testing code will go here
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
     }
   }
 }
