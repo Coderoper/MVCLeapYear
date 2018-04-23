1da0bd23d4c8587161f865633d72be9a5ae928c9
Austin Roberts <d.austin.roberts@gmail.com>
2018-04-22 19:30:49 -0700

    Rearranged project with a test project and main project, builds and tests correctly

---
+++ LeapYear.Tests/ModelTests/LeapYearTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeapYear.Models;
+
+namespace LeapYear.Tests
+{
+  [TestClass]
+  public class LeapYearTest
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
